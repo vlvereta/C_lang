080a83f067c1f57d4403cbc58e30a8bc49e151d9
Vladyslav Vereta <vereta.vlad@gmail.com>
2017-10-05 15:33:25 +0300

    Collatz Conjecture

--- collatz.c
+++ collatz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int 	collatz(int n, int res);
+int 	collatz(int n);
 
 int		main(int argc, char **argv)
 {
@@ -13,20 +13,20 @@ int		main(int argc, char **argv)
 			printf("Please, type the positive integer up to 1000!\n");
 			return (0);
 		}
-		printf("%d\n", collatz(n, 0));
+		printf("%d\n", collatz(n));
 	}
 	else
 		printf("Usage: ./collatz n\n");
 	return (0);
 }
 
-int 	collatz(int n, int res)
+int 	collatz(int n)
 {
 	if (n == 1)
-		return (res);
+		return (0);
 	else if (!(n % 2))
-		return (collatz((n/2), ++res));
+		return (1 + collatz(n/2));
 	else if ((n % 2))
-		return (collatz((3 * n + 1), ++res));
+		return (1 + collatz(3 * n + 1));
 	return (0);
 }
